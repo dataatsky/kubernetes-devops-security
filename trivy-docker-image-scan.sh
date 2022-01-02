7c3be162379ae0b9fe9987eee9feae5eaf12b8bc
victor <35199771+dataatsky@users.noreply.github.com>
2022-01-02 19:29:29 +0100

    Update trivy-docker-image-scan.sh

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -16,4 +16,4 @@ docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit
         exit 1;
     else
         echo "Image scanning passed. No CRITICAL vulnerabilities found"
-    fi;
+    fi; 
